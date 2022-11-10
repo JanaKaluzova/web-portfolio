43ecf13b7eb8ed0507d2407f0b7ec9394b279a8a
Jana Kalužová <jani.kaluzova@gmail.com>
2022-11-10 14:54:56 +0100

    portfolio-images-styles

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -34,9 +34,14 @@ const Portfolio = () => {
                 src={port.cover}
                 alt="portfolio"
                 className="portfolio-image"
-                // width="600"
-                // height="300px"
               />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
             </div>
           );
         })}

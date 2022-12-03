b5a9baf8e096a9a55904e4b6836a24138c0a1d0b
Jana Kalužová <jani.kaluzova@gmail.com>
2022-12-03 12:18:50 +0100

    picture resize fix

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import "./Sidebar.scss";
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../../assets/images/logo.jpg";
+import Logo from "../../assets/images/Logo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
